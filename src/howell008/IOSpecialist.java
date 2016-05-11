0e9f186e8190625f2601b35f999373c759789d1d
fsharafath <sharafath26@gmail.com>
2016-05-11 23:29:41 +0530

    Files Created

---
+++ src/howell008/IOSpecialist.java
@@ -0,0 +1,14 @@
+package howell008;
+
+/**
+ * @author Kevan Buckley, maintained by Matthew Howell
+ * @version 2.0, 2014
+ */
+
+public class IOSpecialist {
+  public IOSpecialist() {   
+  }
+  public String getString(){
+    return IOLibrary.getString();
+  }
+}
---
+++ src/howell008/Preesall.java
@@ -0,0 +1,14 @@
+package howell008;
+public class Preesall {
+  static transient String[] dingo = {
+    "Speaking the Truth in times of universal deceit is a revolutionary act.","George Orwell","",
+    "Do good by stealth, and blush to find it fame.","Alexander Pope","",
+    "Great liars are also great magicians.","Adolf Hitler","",
+    "I viewed my fellow man not as a fallen angel, but as a risen ape.","Desmond Morris","",
+    "As scarce as truth is, the supply has always been in excess of the demand.","Josh Billings","",
+    "Would you live with ease? Do what you ought, not what you please.","Benjamin Franklin","",
+    "Experience is that marvelous thing that enables you to recognize a mistake when you make it again.","Franklin Jones","",
+    "A lie cannot live.","Martin Luther King Jr.","",
+    "Physics is like sex: sure, it may give some practical results, but that's not why we do it.","Richard P. Feynman","",
+    "Truth is sacred; and if you tell the truth too often nobody will believe it.","GK Chesterton",""};
+}
