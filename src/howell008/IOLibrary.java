abcef7143fdaf549db399f5c0bdeb7f3ba1188e1
fsharafath <sharafath26@gmail.com>
2016-06-12 23:52:31 +0530

    Get Ipaddress function removed hence its not used anyware

--- src/howell008/IOLibrary.java
+++ src/howell008/IOLibrary.java
@@ -1,6 +1,5 @@
 package howell008;
 import java.io.*;
-import java.net.*;
 /**
  * @author Kevan Buckley, maintained by Matthew Howell
  * @version 2.0, 2014
@@ -12,18 +11,6 @@ public final class IOLibrary {
     do {
       try {
         return r.readLine();
-      } catch (Exception e) {
-      }
-    } while (true);
-  }
-
-  public static InetAddress getIPAddress() {
-    BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-    do {
-      try {
-        String[] chunks = r.readLine().split("\\.");
-        byte[] data = { Byte.parseByte(chunks[0]),Byte.parseByte(chunks[1]),Byte.parseByte(chunks[2]),Byte.parseByte(chunks[3])};
-        return Inet4Address.getByAddress(data);
       } catch (Exception e) {
       }
     } while (true);
