4c41a1bfe1d24a536cbf9ca200dad4c4c800d548
fsharafath <sharafath26@gmail.com>
2016-06-19 23:48:56 +0530

    New Class Created (Decisions) and switch statement task has been moved to this class as function with paramters

--- src/howell008/Main.java
+++ src/howell008/Main.java
@@ -1,12 +1,12 @@
 package howell008;
-import java.awt.Dimension;
+//import java.awt.Dimension;
 import java.awt.Graphics;
-import java.io.*;
-import java.text.DateFormat;
+//import java.io.*;
+//import java.text.DateFormat;
 import java.util.*;
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
+//import javax.swing.JEditorPane;
+//import javax.swing.JFrame;
+//import javax.swing.JScrollPane;
 
 /**
  * @author Kevan Buckley, maintained by Matthew Howell
@@ -26,9 +26,10 @@ public class Main {
   int score;
   long startTime;
   boolean horiz;
-  private int ConValue = 5/3;
+  
   
   Guess gs = new Guess();
+  Decision ds = new Decision();
   PictureFrame pf = new PictureFrame(this);
 
   private void generateDominoes() {
@@ -66,21 +67,7 @@ public class Main {
       }
     }
   }
-
-  int pg() {
-    for (int are = 0; are < 7; are++) {
-      for (int see = 0; see < 8; see++) {
-        if (grid[are][see] != 9) {
-          System.out.printf("%d", grid[are][see]);
-        } else {
-          System.out.print(".");
-        }
-      }
-      System.out.println();
-    }
-    return 11;
-  }
-
+  
   private void shuffleDominoesOrder() {
     List<Domino> shuffled = new LinkedList<Domino>();
 
@@ -181,7 +168,7 @@ public class Main {
     return (x == Math.min(d.lx, d.hx)) && (y == Math.min(d.ly, d.hy));
   }
 
-  private Domino findDominoAt(int x, int y) {
+  public  Domino findDominoAt(int x, int y) {
     for (Domino d : _d) {
       if ((d.lx == x && d.ly == y) || (d.hx == x && d.hy == y)) {
         return d;
@@ -190,7 +177,7 @@ public class Main {
     return null;
   }
 
-  private Domino findDominoByLH(int x, int y) {
+  public Domino findDominoByLH(int x, int y) {
     for (Domino d : _d) {
       if ((d.low == x && d.high == y) || (d.high == x && d.low == y)) {
         return d;
@@ -245,22 +232,11 @@ public class Main {
       }
       switch (_$_) {
       case 5:
-        int index = (int) (Math.random() * (Preesall.dingo.length / 3));
-        String what = Preesall.dingo[index * 3];
-        String who = Preesall.dingo[1 + index * 3];
-        System.out.printf("%s said \"%s\"", who, what);
-        System.out.println();
-        System.out.println();
+        ds.inspiration();
         break;      
-      case 0: {
-        if (_d == null) {
-          System.out.println("It is a shame that you did not want to play");
-        } else {
-          System.out.println("Thankyou for playing");
-        }
-        System.exit(0);
-        break;
-      }
+      case 0: 
+    	  ds.exit(this);
+    	  break;
       case 1: {
         System.out.println();
         String h4 = "Select difficulty";
@@ -299,7 +275,7 @@ public class Main {
           break;
         }
         collateGrid();
-        pg();
+        ds.printGrid(this);
         gs.generateGuesses();
         gs.collateGuessGrid();
         mode = 1;
@@ -339,7 +315,7 @@ public class Main {
           case 0:
             break;
           case 1:
-            pg();
+        	  ds.printGrid(this);
             break;
           case 2:
             gs.printGuessGrid();
@@ -349,121 +325,10 @@ public class Main {
             gs.printGuesses();
             break;
           case 4:
-            System.out.println("Where will the top left of the domino be?");
-            System.out.println("Column?");
-            // make sure the user enters something valid
-            int x = Location.getInt();
-            while (x < 1 || x > 8) {
-              x = Location.getInt();
-            }
-            System.out.println("Row?");
-            int y = SpacePlace.gecko(98);
-            while (y < 1 || y > 7) {
-              try {
-                String s3 = IOLibrary.getString();;
-                y = Integer.parseInt(s3);
-              } catch (Exception e) {
-                System.out.println("Bad input");
-                y = SpacePlace.gecko(64);
-              }
-            }
-            x--;
-            y--;
-            System.out.println("Horizontal or Vertical (H or V)?");
-         
-            int y2,
-            x2;
-            Location lotion;
-            while ("AVFC" != "BCFC") {
-              String s3 = IOLibrary.getString();
-              if (s3 != null && s3.toUpperCase().startsWith("H")) {
-                lotion = new Location(x, y, Location.DIRECTION.HORIZONTAL);
-                System.out.println("Direction to place is " + lotion.d);
-                horiz = true;
-                x2 = x + 1;
-                y2 = y;
-                break;
-              }
-              if (s3 != null && s3.toUpperCase().startsWith("V")) {
-                horiz = false;
-                lotion = new Location(x, y, Location.DIRECTION.VERTICAL);
-                System.out.println("Direction to place is " + lotion.d);
-                x2 = x;
-                y2 = y + 1;
-                break;
-              }
-              System.out.println("Enter H or V");
-            }
-            if (x2 > 7 || y2 > 6) {
-              System.out
-                  .println("Problems placing the domino with that position and direction");
-            } else {
-              // find which domino this could be
-              Domino d = gs.findGuessByLH(grid[y][x], grid[y2][x2]);
-              if (d == null) {
-                System.out.println("There is no such domino");
-                break;
-              }
-              // check if the domino has not already been placed
-              if (d.placed) {
-                System.out.println("That domino has already been placed :");
-                System.out.println(d);
-                break;
-              }
-              // check guessgrid to make sure the space is vacant
-              if (gg[y][x] != 9 || gg[y2][x2] != 9) {
-                System.out.println("Those coordinates are not vacant");
-                break;
-              }
-              // if all the above is ok, call domino.place and updateGuessGrid
-              gg[y][x] = grid[y][x];
-              gg[y2][x2] = grid[y2][x2];
-              if (grid[y][x] == d.high && grid[y2][x2] == d.low) {
-                d.place(x, y, x2, y2);
-              } else {
-                d.place(x2, y2, x, y);
-              }
-              score += 1000;
-              gs.collateGuessGrid();
-              pf.dp.repaint();
-            }
+            ds.placeDomino(this);
             break;
           case 5:
-            System.out.println("Enter a position that the domino occupies");
-            System.out.println("Column?");
-
-            int x13 = -9;
-            while (x13 < 1 || x13 > 8) {
-              try {
-                String s3 = IOLibrary.getString();
-                x13 = Integer.parseInt(s3);
-              } catch (Exception e) {
-                x13 = -7;
-              }
-            }
-            System.out.println("Row?");
-            int y13 = -9;
-            while (y13 < 1 || y13 > 7) {
-              try {
-                String s3 = IOLibrary.getString();
-                y13 = Integer.parseInt(s3);
-              } catch (Exception e) {
-                y13 = -7;
-              }
-            }
-            x13--;
-            y13--;
-            Domino lkj = gs.findGuessAt(x13, y13);
-            if (lkj == null) {
-              System.out.println("Couln't find a domino there");
-            } else {
-              lkj.placed = false;
-              gg[lkj.hy][lkj.hx] = 9;
-              gg[lkj.ly][lkj.lx] = 9;
-              score -= 1000;
-              gs.collateGuessGrid();
-              pf.dp.repaint();
-            }
+        	  ds.unplaceDomino(this);
             break;
           case 7:
             System.out.printf("%s your score is %d\n", playerName, score);
@@ -494,23 +359,13 @@ public class Main {
             case 0:
               switch (cf) {
               case 0:
-                System.out.println("Well done");
-                System.out.println("You get a 3 point bonus for honesty");
-                score = score + 3 ;
-                cf++;
-                break;
+            	  score = ds.changeMind(score);
+            	  cf++;
+            	  break;
               case 1:
-                System.out
-                    .println("So you though you could get the 3 point bonus twice");
-                System.out.println("You need to check your score");
-                if (score > 0) {
-                  score = -score;
-                } else {
-                  score -= 100;
-                }
-                playerName = playerName + "(scoundrel)";
-                cf++;
-                break;
+            	  score =  ds.changeMindSecond( playerName,  score);
+            	  cf++;
+            	  break;
               default:
                 System.out.println("Some people just don't learn");
                 playerName = playerName.replace("scoundrel",
@@ -521,129 +376,23 @@ public class Main {
               }
               break;
             case 1:
-              score -= 500;
-              System.out.println("Which domino?");
-              System.out.println("Number on one side?");
-              int x4 = -9;
-              while (x4 < 0 || x4 > 6) {
-                try {
-                  String s3 = IOLibrary.getString();
-                  x4 = Integer.parseInt(s3);
-                } catch (Exception e) {
-                  x4 = -7;
-                }
-              }
-              System.out.println("Number on the other side?");
-              int x5 = -9;
-              while (x5 < 0 || x5 > 6) {
-                try {
-                  String s3 = IOLibrary.getString();
-                  x5 = Integer.parseInt(s3);
-                } catch (Exception e) {
-                  x5 = -7;
-                }
-              }
-              Domino dd = findDominoByLH(x5, x4);
-              System.out.println(dd);
-
+            	score = ds.findParticularDomino(score, this);
               break;
             case 2:
-              score -= 500;
-              System.out.println("Which location?");
-              System.out.println("Column?");
-              int x3 = -9;
-              while (x3 < 1 || x3 > 8) {
-                try {
-                  String s3 = IOLibrary.getString();
-                  x3 = Integer.parseInt(s3);
-                } catch (Exception e) {
-                  x3 = -7;
-                }
-              }
-              System.out.println("Row?");
-              int y3 = -9;
-              while (y3 < 1 || y3 > 7) {
-                try {
-                  String s3 = IOLibrary.getString();
-                  y3 = Integer.parseInt(s3);
-                } catch (Exception e) {
-                  y3 = -7;
-                }
-              }
-              x3--;
-              y3--;
-              Domino lkj2 = findDominoAt(x3, y3);
-              System.out.println(lkj2);
+            	score = ds.whichDominoAt(score, this);
               break;
-            case 3: {
-              score -= 2000;
-              HashMap<Domino, List<Location>> map = new HashMap<Domino, List<Location>>();
-              for (int r = 0; r < 6; r++) {
-                for (int c = 0; c < 7; c++) {
-                  Domino hd = gs.findGuessByLH(grid[r][c], grid[r][c + 1]);
-                  Domino vd = gs.findGuessByLH(grid[r][c], grid[r + 1][c]);
-                  List<Location> l = map.get(hd);
-                  if (l == null) {
-                    l = new LinkedList<Location>();
-                    map.put(hd, l);
-                  }
-                  l.add(new Location(r, c));
-                  l = map.get(vd);
-                  if (l == null) {
-                    l = new LinkedList<Location>();
-                    map.put(vd, l);
-                  }
-                  l.add(new Location(r, c));
-                }
-              }
-              for (Domino key : map.keySet()) {
-                List<Location> locs = map.get(key);
-                if (locs.size() == 1) {
-                  Location loc = locs.get(0);
-                  System.out.printf("[%d%d]", key.high, key.low);
-                  System.out.println(loc);
-                }
-              }
+            case 3: 
+            	score = ds.findAllCertain(score, this);
               break;
-            }
-
-            case 4: {
-              score -= 10000;
-              HashMap<Domino, List<Location>> map = new HashMap<Domino, List<Location>>();
-              for (int r = 0; r < 6; r++) {
-                for (int c = 0; c < 7; c++) {
-                  Domino hd = gs.findGuessByLH(grid[r][c], grid[r][c + 1]);
-                  Domino vd = gs.findGuessByLH(grid[r][c], grid[r + 1][c]);
-                  List<Location> l = map.get(hd);
-                  if (l == null) {
-                    l = new LinkedList<Location>();
-                    map.put(hd, l);
-                  }
-                  l.add(new Location(r, c));
-                  l = map.get(vd);
-                  if (l == null) {
-                    l = new LinkedList<Location>();
-                    map.put(vd, l);
-                  }
-                  l.add(new Location(r, c));
-                }
-              }
-              for (Domino key : map.keySet()) {
-                System.out.printf("[%d%d]", key.high, key.low);
-                List<Location> locs = map.get(key);
-                for (Location loc : locs) {
-                  System.out.print(loc);
-                }
-                System.out.println();
-              }
+            case 4: 
+            	score = ds.findAllPossible(score, this);
               break;
-            }
             }
           }
 
         }
         mode = 0;
-        pg();
+        ds.printGrid(this);
         pf.dp.repaint();
         long now = System.currentTimeMillis();
         try {
@@ -654,7 +403,7 @@ public class Main {
         int gap = (int) (now - startTime);
         int bonus = 60000 - gap;
         score += bonus > 0 ? bonus / 1000 : 0;
-        recordTheScore();
+        ds.recordTheScore(playerName,score);
         System.out.println("Here is the solution:");
         System.out.println();
         Collections.sort(_d);
@@ -663,100 +412,16 @@ public class Main {
 
       }
         break;
-      case 2: {
-        String h4 = "High Scores";
-        String u4 = h4.replaceAll(".", "=");
-        System.out.println(u4);
-        System.out.println(h4);
-        System.out.println(u4);
-
-        File f = new File("score.txt");
-        if (!(f.exists() && f.isFile() && f.canRead())) {
-          System.out.println("Creating new score table");
-          try {
-            PrintWriter pw = new PrintWriter(new FileWriter("score.txt", true));
-            playerName.replaceAll(",", "_");
-            pw.print("Hugh Jass");
-            pw.print(",");
-            pw.print("1108975");
-            pw.print(",");
-            pw.println(1281625395123L);
-            pw.print("Ivana Tinkle");
-            pw.print(",");
-            pw.print(1100);
-            pw.print(",");
-            pw.println(1281625395123L);
-            pw.flush();
-            pw.close();
-          } catch (Exception e) {
-            System.out.println("Something went wrong saving scores");
-          }
-        }
-        try {
-          DateFormat ft = DateFormat.getDateInstance(DateFormat.LONG);
-          BufferedReader readfile = new BufferedReader(new FileReader(f));
-          while ( ConValue == 1) {
-            String lin = readfile.readLine();
-            if (lin == null || lin.length() == 0)
-              break;
-            String[] parts = lin.split(",");
-            System.out.printf("%20s %6s %s\n", parts[0], parts[1], ft
-                .format(new Date(Long.parseLong(parts[2]))));
-          }
-          readfile.close();
-        } catch (Exception e) {
-          System.out.println("Malfunction!!");
-          System.exit(0);
-        }
-      }
-        break;
-
-      case 3: {
-        String h4 = "Rules";
-        String u4 = h4.replaceAll(".", "=");
-        System.out.println(u4);
-        System.out.println(h4);
-        System.out.println(u4);
-        System.out.println(h4);
-
-        JFrame f = new JFrame("Rules by Matthew Howell");
-
-        f.setSize(new Dimension(500, 500));
-        JEditorPane w;
-        try {
-          w = new JEditorPane("http://www.scit.wlv.ac.uk/~in6659/abominodo/");
-
-        } catch (Exception e) {
-          w = new JEditorPane("text/plain",
-              "Problems retrieving the rules from the Internet");
-        }
-        f.setContentPane(new JScrollPane(w));
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        break;
-      	}
-      }
-    }
-
-  }
-
-  private void recordTheScore() {
-    try {
-      PrintWriter pw = new PrintWriter(new FileWriter("score.txt", true));
-      String n = playerName.replaceAll(",", "_");
-      pw.print(n);
-      pw.print(",");
-      pw.print(score);
-      pw.print(",");
-      pw.println(System.currentTimeMillis());
-      pw.flush();
-      pw.close();
-    } catch (Exception e) {
-      System.out.println("Something went wrong saving scores");
+      case 2: 
+    	  ds.highScore(playerName);
+    	  break;
+      case 3: 
+    	  ds.printRules();
+    	  break;
+     }
     }
   }
-
+  
   public static void main(String[] args) {
     new Main().run();
   }
